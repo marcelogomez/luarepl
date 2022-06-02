94359cdde4c3827697b85c1aabd7bdb763c6adc0
Marcelo Gomez <marcelogomez321@gmail.com>
2022-06-02 19:08:39 +0100

    Use mpsc channel to mvoe evaluation to a separate thread

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
+use rlua::Error;
 use rlua::Lua;
 use rlua::Value;
-use rlua::Error;
 use std::io::BufRead;
+use std::sync::mpsc::Sender;
+use std::thread;
+use std::thread::JoinHandle;
 
 #[derive(Debug)]
 struct Session {
-    expressions: Vec<String>,
-    lua_instance: Lua,
+    expr_sender: Sender<String>,
+    eval_thread: JoinHandle<()>,
 }
 
 impl Session {
     pub fn new() -> Self {
+        let (expr_sender, expr_receiver) = std::sync::mpsc::channel::<String>();
+        let eval_thread = thread::spawn(move || {
+            let lua = Lua::new();
+            lua.context(|ctx| {
+                for expr in expr_receiver.into_iter() {
+                    println!("{:?}", ctx.load(&expr).eval::<Value>());
+                }
+            });
+        });
+
         Self {
-            expressions: vec![],
-            lua_instance: Lua::new(),
+            expr_sender,
+            eval_thread,
         }
     }
 
     pub fn eval(&mut self, expr: String) {
-        self.expressions.push(expr);
-        self.lua_instance.context(|ctx| {
-            let results = self.expressions
-                .iter()
-                .map(|exp| ctx.load(exp).eval::<Value>())
-                .collect::<Vec<Result<Value, Error>>>();
-            
-            println!("{:#?}", results);
-        });
+        self.expr_sender.send(expr);
     }
 }
 
@@ -35,4 +40,5 @@ fn main() {
     for line in std::io::stdin().lock().lines() {
         session.eval(line.unwrap());
     }
+    session.eval_thread.join();
 }
