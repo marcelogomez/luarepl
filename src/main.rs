4cca3f88c95679e46f9e4ef30dbc5d536750c59a
Marcelo Gomez <marcelogomez321@gmail.com>
2022-06-02 19:25:17 +0100

    Make it work with tokio::spawn for async server (super hacky)

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,35 @@ use rlua::Error;
 use rlua::Lua;
 use rlua::Value;
 use std::io::BufRead;
-use std::sync::mpsc::Sender;
 use std::thread;
-use std::thread::JoinHandle;
+use tokio::sync::mpsc::UnboundedSender;
+use tokio::task::JoinHandle;
 
 #[derive(Debug)]
 struct Session {
-    expr_sender: Sender<String>,
+    expr_sender: UnboundedSender<String>,
     eval_thread: JoinHandle<()>,
 }
 
 impl Session {
     pub fn new() -> Self {
-        let (expr_sender, expr_receiver) = std::sync::mpsc::channel::<String>();
-        let eval_thread = thread::spawn(move || {
+        let (expr_sender, mut expr_receiver) = tokio::sync::mpsc::unbounded_channel();
+        let eval_thread = tokio::spawn(async move {
             let lua = Lua::new();
-            lua.context(|ctx| {
-                for expr in expr_receiver.into_iter() {
-                    println!("{:?}", ctx.load(&expr).eval::<Value>());
-                }
+            let (inner_sender, inner_receiver) = std::sync::mpsc::channel::<String>();
+            let eval_thread = thread::spawn(move || {
+                lua.context(|ctx| {
+                    inner_receiver
+                        .into_iter()
+                        .map(|expr| (ctx.load(&expr).eval::<Value>(), expr))
+                        .for_each(|(result, expr)| println!("{} -> {:?}", expr, result));
+                });
             });
+
+            while let Some(expr) = expr_receiver.recv().await {
+                let _ = inner_sender.send(expr);
+            }
+            let _ = eval_thread.join();
         });
 
         Self {
@@ -30,15 +39,15 @@ impl Session {
         }
     }
 
-    pub fn eval(&mut self, expr: String) {
-        self.expr_sender.send(expr);
+    pub async fn eval(&mut self, expr: String) {
+        let _ = self.expr_sender.send(expr);
     }
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut session = Session::new();
     for line in std::io::stdin().lock().lines() {
-        session.eval(line.unwrap());
+        session.eval(line.unwrap()).await;
     }
-    session.eval_thread.join();
 }
